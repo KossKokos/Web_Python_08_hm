d02c96e6463b4a5761f532a14470fcf4222bef6a
KossKokos <koskokos0@gmail.com>
2023-10-26 17:40:28 +0100

    main commit

--- authors_qoutes/find_info.py
+++ authors_qoutes/find_info.py
@@ -2,7 +2,6 @@ from pprint import pprint
 
 import redis
 from redis_lru import RedisLRU
-
 from models import Authors, Qoutes
 
 client = redis.StrictRedis(host="localhost", port=6379, username='default', password=None)
@@ -13,68 +12,74 @@ cache = RedisLRU(client)
 def authors_name(args):
     fullname = args.strip()
     authors = Authors.objects(fullname__icontains=fullname) # icontains mean insensitive case and contains(like regex)
+    result = []
     for author in authors:
-        pprint(f'fullname: {author.fullname}, born date: {author.born_date}, born location: {author.born_location}\
+        result.append(f'fullname: {author.fullname}, born date: {author.born_date}, born location: {author.born_location}\
             description: {author.description}')
-        print('*'*80)
+    return result
 
 # func gets author's info using born date from mongodb
+@cache
 def authors_b_date(args):
     b_date = args.strip()
     authors = Authors.objects(born_date__icontains=b_date)
+    result = []
     for author in authors:
-        pprint(f'fullname: {author.fullname}\
-                born date: {author.born_date}\
-                born location: {author.born_location}\
-                description: {author.description}') 
-        print('*'*80)
+        result.append(f'fullname: {author.fullname}, born date: {author.born_date}, born location: {author.born_location}\
+            description: {author.description}')
+    return result
 
 # func gets author's info using born location from mongodb
+@cache
 def authors_location(args):
     location = args.strip()
     authors = Authors.objects(born_location__icontains=location)
+    result = []
     for author in authors:
-        pprint(f'fullname: {author.fullname}\
-                born date: {author.born_date}\
-                born location: {author.born_location}\
-                description: {author.description}')
-        print('*'*80)
+        result.append(f'fullname: {author.fullname}, born date: {author.born_date}, born location: {author.born_location}\
+            description: {author.description}')
+    return result
 
 # func gets author's info using description from mongodb
+@cache
 def authors_description(args):
     description = args.strip()
     authors = Authors.objects(description__icontains=description)
+    result = []
     for author in authors:
-        pprint(f'fullname: {author.fullname}\
-                born date: {author.born_date}\
-                born location: {author.born_location}\
-                description: {author.description}') 
-        print('*'*80)
+        result.append(f'fullname: {author.fullname}, born date: {author.born_date}, born location: {author.born_location}\
+            description: {author.description}')
+    return result
 
 # func gets qoute's info using tag from mongodb
 @cache
 def qoutes_tag(args):
     tag = args.strip()
     qoutes = Qoutes.objects(tags__icontains=tag)# single tag
+    result = []
     for qoute in qoutes:
-        pprint(f'tags: {qoute.tags}, author: {qoute.author.fullname}, qoute: {qoute.qoute}')
-        print('*'*80)
+        result.append(f'tags: {qoute.tags}, author: {qoute.author.fullname}, qoute: {qoute.qoute}')
+    return result
 
 # func gets qoute's info using tags from mongodb
+@cache
 def qoutes_tags(args):
     tags = args.strip().split(',')
     qoutes = Qoutes.objects(tags__in__icontains=tags)# list of tags
+    result = []
     for qoute in qoutes:
-        pprint(f'tags: {qoute.tags}, author: {qoute.author.fullname}, qoute: {qoute.qoute}')
-        print('*'*80)
+        result.append(f'tags: {qoute.tags}, author: {qoute.author.fullname}, qoute: {qoute.qoute}')
+    return result
 
 # func gets qoute's info using qoute from mongodb
+@cache
 def qoutes_qoute(args):
     qoute = args.strip()
     qoutes = Qoutes.objects(qoute__icontains=qoute)
-    for qoute in qoutes:        
-        pprint(f'tags: {qoute.tags}, author: {qoute.author.fullname}, qoute: {qoute.qoute}')
-        print('*'*80)
+    result = []
+    for qoute in qoutes:
+        result.append(f'tags: {qoute.tags}, author: {qoute.author.fullname}, qoute: {qoute.qoute}')
+    return result
 
 # dict, where keys are commands(user input) and values are funcs
 commands = {'name': authors_name, 'date': authors_b_date, 'location': authors_location,
@@ -90,7 +95,9 @@ def main():
                 print('Bye')
                 break
             comm, args = user_input.split(':') # parse user_input
-            commands.get(comm)(args) # calling a function using a key
+            result = commands.get(comm)(args) # calling a function using a key
+            for res in result:
+                print(res)
         except TypeError:
             print('Wrong command')
         except ValueError:
